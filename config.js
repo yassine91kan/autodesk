677aa2420b1f957a2863d206bdc4497754ae7c1d
yassine91 <kandoussi.yassine@gmail.com>
2024-01-02 21:29:23 -0500

    autodesk v1.1

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
     process.exit(1);
 }
 APS_BUCKET = APS_BUCKET || `${APS_CLIENT_ID.toLowerCase()}-basic-app`;
-PORT = PORT || 8080;
+PORT = process.env.PORT || 8080;
 
 module.exports = {
     APS_CLIENT_ID,
--- wwwroot/extensions/SummaryExtension.js
+++ wwwroot/extensions/SummaryExtension.js
@@ -47,6 +47,122 @@ class SummaryExtension extends BaseExtension {
     onModelLoaded(model) {
         super.onModelLoaded(model);
         this.update();
+
+        console.log("I am trying here buddy");
+
+        function queryModelMetadata(urn, access_token) {
+            fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Metadata:", data);
+                // Continue with retrieving an object tree or properties
+            })
+            .catch(error => {
+                console.error("Error querying model's metadata:", error);
+            });
+        }
+
+
+        function retrieveObjectTree(urn, guid, access_token) {
+            fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata/${guid}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Object Tree:", data);
+            })
+            .catch(error => {
+                console.error("Error retrieving object tree:", error);
+            });
+        }
+
+        let query = {
+           
+            "query": {
+                "$contains": [
+                    "properties.Materials and Finishes.Structural Material",
+                    "Concrete"
+                ]
+            },
+            "fields": [
+                "objectid",
+                "name",
+                "externalId",
+                "properties.Materials and Finishes"
+            ],
+            "pagination": {
+                "offset": 30,
+                "limit": 30
+            },
+            "payload": "text"
+
+        };
+
+        console.log(JSON.stringify(query))
+
+        function queryModel(urn, guid, access_token) {
+            fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata/${guid}/properties:query`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify(query)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Object Tree:", data);
+            })
+            .catch(error => {
+                console.error("Error retrieving object tree:", error);
+            });
+        }
+
+
+//
+        const client_id = 'fQPtGtGD67GG4gVubAGeIvjPXUsuOimS';
+        const client_secret = 'Wrwo4ROxNFlfrYmG';
+        const credentials = `client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials&scope=data:read`;
+
+        fetch('https://developer.api.autodesk.com/authentication/v1/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: credentials
+        })
+        .then(response => response.json())
+        .then(data => {
+        const access_token = data.access_token;
+        console.log("You access token is :");    
+        console.log(access_token);
+         // Proceed to query the model's metadata
+         let urn = "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6ZnFwdGd0Z2Q2N2dnNGd2dWJhZ2VpdmpweHVzdW9pbXMtYmFzaWMtYXBwL3JzdGJhc2ljc2FtcGxlcHJvamVjdC5ydnQ";
+         let guid = "2b8b1cf8-31bf-7e71-dfb5-e1d4342ddb82";
+        //  queryModelMetadata(urn,access_token)
+        // retrieveObjectTree(urn, guid, access_token)
+        queryModel(urn, guid, access_token)
+
+        })
+        .catch(error => {
+            console.error("Error authenticating:", error);
+        });
+
+        // Metadata EndPoint    
+        
+
+
+
+//
+
     }
 
     onSelectionChanged(model, dbids) {
@@ -63,6 +179,15 @@ class SummaryExtension extends BaseExtension {
         if (this._panel) {
             const selectedIds = this.viewer.getSelection();
             const isolatedIds = this.viewer.getIsolatedNodes();
+        // Added Code 
+
+     
+
+        //
+
+
+
+
             if (selectedIds.length > 0) { // If any nodes are selected, compute the aggregates for them
                 this._panel.update(this.viewer.model, selectedIds, SUMMARY_PROPS);
             } else if (isolatedIds.length > 0) { // Or, if any nodes are isolated, compute the aggregates for those
